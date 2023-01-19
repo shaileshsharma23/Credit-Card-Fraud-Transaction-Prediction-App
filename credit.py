3d9717232b2434d60d62a587b464dbb7cb7c3674
Shailesh Sharma <122387682+shaileshsharma23@users.noreply.github.com>
2023-01-19 13:06:07 +0530

    Update credit.py

--- credit.py
+++ credit.py
@@ -86,8 +86,7 @@ def main():
     #creating button
     
     if st.button("result"):
-        card_fraud=card_pred([Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,
-    	V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount])
+        card_fraud=card_pred([Time,V1,V2,V3,V4,V5,V6,V7,V8,V9,V10,V11,V12,V13,V14,V15,V16,V17,V18,V19,V20,V21,V22,V23,V24,V25,V26,V27,V28,Amount])
         
     st.success(card_fraud)
     
